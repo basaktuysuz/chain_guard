38066232c5768d568e00550a87d396c14dc6202a
basaktuysuz1 <85128377+basaktuysuz@users.noreply.github.com>
2024-05-18 22:30:50 +0300

    Packages details added

--- lib/src/view/screen/package_detail.dart
+++ lib/src/view/screen/package_detail.dart
@@ -11,18 +11,13 @@ class PackageDetailsPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            buildPackageCard('Package 1', 'Content 1', '5 kg', 'ID1', context),
-            buildPackageCard('Package 2', 'Content 2', '10 kg', 'ID2', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
-            buildPackageCard('Package 3', 'Content 3', '8 kg', 'ID3', context),
+            buildPackageCard('Package 1', 'aeghe24-gajds', 'Kıyafet', 'Günlük Giyim', "Org1",'15*15*30', '12', 'Ankara-Trabzon', context),
+            buildPackageCard('Package 2', 'wefwef34-43', 'Yiyecek', 'Konserve Gıda', "Org1",'15*15*30', '12', 'Kayseri-Adana', context),
+            buildPackageCard('Package 3', 'dfsdf567-65sdsdv', 'Kıyafet', 'Kışlık Giyim',"Org1", '15*15*30', '12', 'Bolu-Hakkari', context),
+            buildPackageCard('Package 4', 'dsd78hns-80v', 'Medikal Malzeme', 'İlaç',"Org1", '20*10*10', '5', 'İstanbul-İzmir', context),
+            buildPackageCard('Package 5', 'abc43dstgs80v', 'Temizlik Ürünleri', 'Hijyen Ürünleri', "Org1",'25*25*25', '10', 'Antalya-Bursa', context),
+            buildPackageCard('Package 6', 'c4dsdd-ts8ve', 'Barınma Malzemeleri', 'Çadır',"Org1", '50*50*50', '15', 'Gaziantep-Samsun', context),
+            buildPackageCard('Package 7', 'rvvy6t-dfgu', 'Barınma Malzemeleri', 'Battaniye', "Org1",'40*40*20', '8', 'Diyarbakır-Malatya', context),
 
           ],
         ),
@@ -30,27 +25,53 @@ class PackageDetailsPage extends StatelessWidget {
     );
   }
 
-  Widget buildPackageCard(String packageName, String packageContent, String packageWeight, String packageId, BuildContext context) {
+  Widget buildPackageCard(String packageName, String packageId, String packageContent, String packageComment,  String organizationName,String packageSize, String packageWeight, String packageRoute, BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
       elevation: 4,
       margin: EdgeInsets.all(8),
-      child: ListTile(
-        title: Text(packageName),
-        subtitle: Column(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Content: $packageContent'),
-            Text('Weight: $packageWeight'),
-            Text('ID: $packageId'),
-            // Add more package details as needed
+            Row(
+              children: [
+                Icon(Icons.local_shipping, color: Colors.blue),
+                SizedBox(width: 8),
+                Text(packageName, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 8),
+            buildDetailRow(Icons.qr_code, 'ID', packageId),
+            buildDetailRow(Icons.inventory, 'Content', packageContent),
+            buildDetailRow(Icons.style, 'Comment', packageComment),
+            buildDetailRow(Icons.business, 'Organization', packageComment),
+            buildDetailRow(Icons.straighten, 'Package Size', packageSize),
+            buildDetailRow(Icons.line_weight, 'Weight', packageWeight),
+            buildDetailRow(Icons.alt_route, 'Transfer Route', packageRoute),
           ],
         ),
-        onTap: () {
-          showOptionsDialog(context, packageName);
-        },
       ),
     );
   }
+
+  Widget buildDetailRow(IconData icon, String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.grey),
+          SizedBox(width: 8),
+          Text('$label: ', style: TextStyle(fontWeight: FontWeight.bold)),
+          Expanded(child: Text(value)),
+        ],
+      ),
+    );
+  }
+
 
   void showOptionsDialog(BuildContext context, String packageName) {
     showDialog(
